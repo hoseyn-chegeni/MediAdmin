9a3cfa9d9b7f6f2416dfc8a36eb1e12b818d1d3a
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-02-08 21:55:29 +0330

    User Detail View done (#7)

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserListView
+from .views import UserListView,UserDetailView
 
 app_name = "accounts"
 
 urlpatterns = [
-    path("user_list/", UserListView.as_view(), name="user_list"),
+    path("user/list/", UserListView.as_view(), name="user_list"),
+    path("user/detail/<int:pk>/", UserDetailView.as_view(), name="user_detail"),
 ]
--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,18 +1,22 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from .models import User
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView,UpdateView,DeleteView
 
 
 # Create your views here.
 class UserListView(ListView):
     model = User
-    template_name = "accounts/user_list.html"
+    template_name = "accounts/list.html"
     permission_required = "accounts.view_user"
     context_object_name = "users"
 
 
-class UserDetailView:
-    pass
+class UserDetailView(DetailView):
+    model = User
+    template_name = "accounts/detail.html"
+    context_object_name = "user"
 
 
 class UserCreateView:
