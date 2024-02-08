1e22b0913006cb7fbdb6385cf92e8784af5d45ad
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-02-08 21:35:21 +0330

    add userListView(#7)

---
+++ core/accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import UserListView
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('user_list/',UserListView.as_view(),name="user_list"),
+]
--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import User
+from django.views.generic import ListView
 
 # Create your views here.
+class UserListView(ListView):
+    model = User
+    template_name = "accounts/user_list.html"
+    permission_required = "accounts.view_user"
+    context_object_name = "users"
+
+
+class UserDetailView():
+    pass
+
+class UserCreateView():
+    pass
+
+class UserUpdateView():
+    pass
+
+class UserDeleteView():
+    pass
