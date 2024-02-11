f013e46de2c7256b2bde0e4783c7fe9a18d8d0b7
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-02-11 18:52:06 +0330

    add client model

--- core/client/models.py
+++ core/client/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Client(models.Model):
+    case_id = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    fathers_name = models.CharField(max_length=100)
+    national_id = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    phone_number = models.CharField(max_length=15)
+    address = models.TextField()
+    MARITAL_STATUS_CHOICES = (
+        ('S', 'Single'),
+        ('M', 'Married'),
+    )
+    marital_status = models.CharField(max_length=1, choices=MARITAL_STATUS_CHOICES)
+    emergency_contact_name = models.CharField(max_length=100)
+    emergency_contact_number = models.CharField(max_length=15)
