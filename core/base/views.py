9078ff093f8aef390ce8dc34a41835fd85a83d7f
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-02-11 11:45:26 +0330

    add login requiredMixin to base views (#12)

--- core/base/views.py
+++ core/base/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DeleteView,DetailView,CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
+class BaseListView(LoginRequiredMixin, ListView):
+    pass
+
+class BaseCreateView(LoginRequiredMixin, CreateView):
+    pass
+
+class BaseDetailView(LoginRequiredMixin, DetailView):
+    pass
+
+class BaseUpdateView(LoginRequiredMixin, UpdateView):
+    pass
+
+class BaseDeleteView(LoginRequiredMixin, DeleteView):
+    pass
+
+
