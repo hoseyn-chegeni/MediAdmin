2e3d3889af4aa16a951c04507da39294c90b00b0
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-02-12 10:57:51 +0330

    add reception model (#17)

--- core/reception/models.py
+++ core/reception/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Reception(models.Model):
+    client = models.ForeignKey('client.Client', on_delete=models.CASCADE)
+    reason = models.CharField(max_length=255)
+    PAYMENT_TYPE_CHOICES = (
+        ('CASH', 'Cash'),
+        ('CARD', 'Card'),
+        ('INSURANCE', 'Insurance'),
+        ('OTHER', 'Other'),
+    )
+    payment_type = models.CharField(max_length=10, choices=PAYMENT_TYPE_CHOICES)
+    PAYMENT_STATUS_CHOICES = (
+        ('PAID', 'Paid'),
+        ('PENDING', 'Pending'),
+        ('UNPAID', 'Unpaid'),
+    )
+    payment_status = models.CharField(max_length=10, choices=PAYMENT_STATUS_CHOICES)
+    prescription = models.TextField()
+
+    def __str__(self):
+        return f'Reception for {self.client.first_name} {self.client.last_name}'
