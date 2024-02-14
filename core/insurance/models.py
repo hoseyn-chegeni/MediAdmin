c374ee32fd9206cb8ac35f4a81366d891249ea7a
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-02-14 15:29:59 +0330

    add insurance model (#20)

--- core/insurance/models.py
+++ core/insurance/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Insurance(models.Model):
+    name = models.CharField(max_length = 255)
+    policy_number = models.CharField(max_length=50)
+    insurance_company = models.CharField(max_length=100)
+    deductible = models.DecimalField(max_digits=10, decimal_places=2)
+    copay = models.DecimalField(max_digits=10, decimal_places=2)
+    max_annual_coverage = models.DecimalField(max_digits=10, decimal_places=2)
+    policy_type = models.CharField(max_length=50)
+    notes = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
