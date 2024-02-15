9af9ed8785e7f8b055a620a3f3d43fcd41cc7d46
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-02-15 13:59:19 +0330

    add services model (#23)

--- core/services/models.py
+++ core/services/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Service(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    category = models.CharField(max_length=100)
+    duration = models.PositiveIntegerField()  # Duration in minutes
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    availability = models.BooleanField(default = True)
+    equipment_supplies = models.TextField(blank=True, null=True)
+    preparation_instructions = models.TextField(blank=True, null=True)
+    documentation_requirements = models.TextField(blank=True, null=True)
+    notes = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.name
