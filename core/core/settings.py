d767fc7bc0da57a24fd51cf4bc8e765a59498140
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-02-21 15:22:33 +0330

    add doctors app

--- core/core/settings.py
+++ core/core/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     "insurance",
     "services",
     "asset",
+    "doctor"
 ]
 
 MIDDLEWARE = [
--- core/core/urls.py
+++ core/core/urls.py
@@ -33,6 +33,8 @@ urlpatterns = (
         path("insurance/", include("insurance.urls")),
         path("services/", include("services.urls")),
         path("asset/", include("asset.urls")),
+        path("doctor/", include("doctor.urls")),
+
     ]
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
