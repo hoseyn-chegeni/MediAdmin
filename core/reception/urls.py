6bebfb3c7ce23c72752e4d74bc171f50eb26b6a4
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-02-22 10:55:04 +0330

    add reception from profile (#17)

--- core/reception/urls.py
+++ core/reception/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path 
-from .views import ReceptionCreateView, ReceptionListView, ReceptionDetailView
+from .views import ReceptionCreateView, ReceptionListView, ReceptionDetailView,ReceptionCreateViewUsingProfile
 
 app_name = 'reception'
 
@@ -7,4 +7,5 @@ urlpatterns =[
     path("list/", ReceptionListView.as_view(), name="list"),
     path("detail/<int:pk>/", ReceptionDetailView.as_view(), name="detail"),
     path("create/", ReceptionCreateView.as_view(), name="create"),
+    path('create/<int:pk>/', ReceptionCreateViewUsingProfile.as_view(), name='reception_create_using_profile'),
 ]
--- core/reception/views.py
+++ core/reception/views.py
@@ -32,3 +32,25 @@ class ReceptionDetailView(BaseDetailView):
     model = Reception
     template_name = 'reception/detail.html'
     context_object_name = 'reception'
+
+
+class ReceptionCreateViewUsingProfile(BaseCreateView):
+    model = Reception
+    fields =[
+        "reason","payment_type","payment_status"
+    ]
+    template_name = "reception/create_profile.html"
+
+    def get_success_url(self):
+        return reverse_lazy("reception:detail", kwargs={"pk": self.object.pk})
+    
+    def get_initial(self):
+        initial = super().get_initial()
+        # Set the initial client value to the client whose profile page the user is on
+        initial['client'] = self.kwargs['pk']  # Assuming client's pk is passed in the URL
+        return initial
+
+    def form_valid(self, form):
+        # Set the client for the reception
+        form.instance.client_id = self.kwargs['pk']  # Assuming client's pk is passed in the URL
+        return super().form_valid(form)
