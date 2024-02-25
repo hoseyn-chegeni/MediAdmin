f109524cc677b82a9db27a53e12dfa1c4cb7abb1
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-02-25 14:03:13 +0330

    add age func in client model

--- core/client/models.py
+++ core/client/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import date
 
 
 # Create your models here.
@@ -40,6 +41,14 @@ class Client(models.Model):
         "insurance.Insurance", on_delete=models.CASCADE, blank=True, null=True
     )
     is_vip = models.BooleanField(default=False)
+
+    @property
+    def age(self):
+        today = date.today()
+        age = today.year - self.date_of_birth.year - ((today.month, today.day) < (self.date_of_birth.month, self.date_of_birth.day))
+        return age
+
+    
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
