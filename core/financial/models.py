6b9f09fb341e2f65391f86384070f2f3bff058d8
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-02-29 12:52:10 +0330

    add insurance_amount and final amount to financial instance

--- core/financial/models.py
+++ core/financial/models.py
@@ -8,6 +8,7 @@ class Financial(models.Model):
     reception = models.OneToOneField("reception.Reception", on_delete=models.CASCADE)
     valid_insurance = models.BooleanField(default = False)
     insurance_range = models.PositiveIntegerField(default = 0)
+    insurance_amount = models.DecimalField(max_digits=10, decimal_places=2, default = 0)  
     date_issued = models.DateField(auto_now_add=True)
     service_price = models.DecimalField(
         max_digits=10, decimal_places=2, blank=True, null=True
@@ -16,6 +17,9 @@ class Financial(models.Model):
         max_digits=10, decimal_places=2, blank=True, null=True
     )
     total_amount = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=True, null=True
+    )
+    final_amount = models.DecimalField(
         max_digits=10, decimal_places=2, blank=True, null=True
     )
     payment_status = models.CharField(
@@ -42,7 +46,9 @@ class Financial(models.Model):
             total_amount_before_tax = self.service_price + self.consumable_price
             total_amount_after_tax = total_amount_before_tax * (1 + self.tax_rate)
             total_amount_with_insurance = total_amount_after_tax - (total_amount_after_tax * (Decimal(str(self.insurance_range)) / Decimal(100)))
-            self.total_amount = total_amount_with_insurance
+            self.total_amount = total_amount_after_tax
+            self.insurance_amount = total_amount_after_tax - total_amount_with_insurance
+            self.final_amount = total_amount_with_insurance
         # Check if service price or consumable price has been modified
         if "service_price" in kwargs.get(
             "update_fields", []
