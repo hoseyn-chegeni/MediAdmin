8fe8057673abc2ce823ad6a8b57c059061f7c998
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-03-04 17:08:42 +0330

    each service only can have 3 appointment per day

--- core/booking/models.py
+++ core/booking/models.py
@@ -23,7 +23,7 @@ def validate_max_appointments_per_day(value):
 class Appointment(models.Model):
     service = models.ForeignKey('services.Service', on_delete = models.CASCADE)
     client = models.ForeignKey("client.Client", on_delete = models.CASCADE)
-    date = models.DateField(validators=[validate_current_month, validate_max_appointments_per_day])
+    date = models.DateField()
 
     def __str__(self):
         return f'{self.service}, {self.client}, {self.date}'
--- core/booking/views.py
+++ core/booking/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from base.views import BaseCreateView
 from .models import Appointment
 from django.urls import reverse_lazy
+from datetime import datetime
 
 # Create your views here.
 class AppointmentCreateView(BaseCreateView):
@@ -12,6 +13,13 @@ class AppointmentCreateView(BaseCreateView):
         return reverse_lazy('index:index')
 
     def form_valid(self, form):
+        appointment_date = form.cleaned_data['date']
+        current_month = datetime.now().month
+        appointment_month = appointment_date.month
+        if current_month != appointment_month:
+            form.add_error('date', "Appointments can only be made for the current month.")
+            return self.form_invalid(form)
+
         service = form.instance.service
         date = form.instance.date
         client = form.instance.client
@@ -19,4 +27,13 @@ class AppointmentCreateView(BaseCreateView):
         if existing_appointments_count > 0:
             form.add_error(None, "you can only book one visit for a doctor in a day ")
             return self.form_invalid(form)
+        
+
+        service = form.cleaned_data['service']
+        date = form.cleaned_data['date']
+        appointments_count = Appointment.objects.filter(service=service, date=date).count()
+        if appointments_count >= 3:
+            form.add_error('service', "This service already has maximum appointments for this date.")
+            return self.form_invalid(form)
+
         return super().form_valid(form)
