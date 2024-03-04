c05cdd1af3746dfa162565894120e09151259f63
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-03-04 10:58:35 +0330

    add appointment model

--- core/booking/models.py
+++ core/booking/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Appointment(models.Model):
+    service = models.ForeignKey('services.Service', on_delete = models.CASCADE)
+    client = models.ForeignKey("client.Client", on_delete = models.CASCADE)
+    date = models.DateField()
+
+    def __str__(self):
+        return f'{self.service}, {self.client}, {self.date}'
