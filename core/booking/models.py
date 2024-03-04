bf86c410a51cfeac6152550313724de96f9810ca
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-03-04 11:09:33 +0330

    add validate_max_appointments_per_day to appointment model

--- core/booking/models.py
+++ core/booking/models.py
@@ -10,10 +10,17 @@ def validate_current_month(value):
     if value.month != today.month or value.year != today.year:
         raise ValidationError("Date must be in the current month.")
     
+def validate_max_appointments_per_day(value):
+    # Count existing appointments for the given date
+    existing_count = Appointment.objects.filter(date=value).count()
+    if existing_count >= 3:
+        raise ValidationError("Only three appointments are allowed per day.")
+
+    
 class Appointment(models.Model):
     service = models.ForeignKey('services.Service', on_delete = models.CASCADE)
     client = models.ForeignKey("client.Client", on_delete = models.CASCADE)
-    date = models.DateField(validators=[validate_current_month])
+    date = models.DateField(validators=[validate_current_month, validate_max_appointments_per_day])
 
     def __str__(self):
         return f'{self.service}, {self.client}, {self.date}'
