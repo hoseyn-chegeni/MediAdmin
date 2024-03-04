0da1aea57b58aa59f316a3a68f3430026860423e
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-03-04 11:07:01 +0330

    add date validator to appointment model

--- core/booking/models.py
+++ core/booking/models.py
@@ -1,10 +1,20 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+from datetime import datetime
+
 
 # Create your models here.
+def validate_current_month(value):
+    today = timezone.now()
+    if value.month != today.month or value.year != today.year:
+        raise ValidationError("Date must be in the current month.")
+    
 class Appointment(models.Model):
     service = models.ForeignKey('services.Service', on_delete = models.CASCADE)
     client = models.ForeignKey("client.Client", on_delete = models.CASCADE)
-    date = models.DateField()
+    date = models.DateField(validators=[validate_current_month])
 
     def __str__(self):
         return f'{self.service}, {self.client}, {self.date}'
+    
