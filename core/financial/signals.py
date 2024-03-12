c7c3902d559bbca9d7b55a2272abf2acd6b4b4b3
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-03-12 11:21:44 +0330

    fix reception update issue

--- core/financial/signals.py
+++ core/financial/signals.py
@@ -12,21 +12,21 @@ def create_financial(sender, instance, created, **kwargs):
         service = instance.service
         service_insurance_services = InsuranceService.objects.filter(service=service)
 
-    if service_insurance_services:
-        for i in service_insurance_services:
-            if i.id == client_insurance.id:
-                Financial.objects.create(
-                    reception=instance,
-                    invoice_number=f"INV-{instance.pk}",  # You can customize how the invoice number is generated
-                    payment_status="UNPAID",  # Default payment status
-                    payment_received_date=None,  # No payment received initially
-                    valid_insurance=True,
-                    insurance_range=i.percentage,
-                )
-    else:
-        Financial.objects.create(
-            reception=instance,
-            invoice_number=f"INV-{instance.pk}",  # You can customize how the invoice number is generated
-            payment_status="UNPAID",  # Default payment status
-            payment_received_date=None,  # No payment received initially
-        )
+        if service_insurance_services:
+            for i in service_insurance_services:
+                if i.id == client_insurance.id:
+                    Financial.objects.create(
+                        reception=instance,
+                        invoice_number=f"INV-{instance.pk}",  # You can customize how the invoice number is generated
+                        payment_status="UNPAID",  # Default payment status
+                        payment_received_date=None,  # No payment received initially
+                        valid_insurance=True,
+                        insurance_range=i.percentage,
+                    )
+        else:
+            Financial.objects.create(
+                reception=instance,
+                invoice_number=f"INV-{instance.pk}",  # You can customize how the invoice number is generated
+                payment_status="UNPAID",  # Default payment status
+                payment_received_date=None,  # No payment received initially
+            )
