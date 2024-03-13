974f936bb58df2435098b589da82621bd0e15220
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-03-13 17:56:36 +0330

    edit signal for update reception in day

--- core/reception/signals.py
+++ core/reception/signals.py
@@ -19,8 +19,9 @@ def update_last_reception_date(sender, instance, created, **kwargs):
 def update_reception_number(sender, instance, created, **kwargs):
     if created:
         today = date.today()
-        reception_number = Reception.objects.filter(date = today).count()
+        reception_number = Reception.objects.filter(date = today, service = instance.service).count()
         instance.reception_in_day = reception_number 
         instance.save()
 
 
+ 
