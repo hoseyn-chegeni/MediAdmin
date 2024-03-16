7973f32cc4041bcfdc191f400f13ce0fafee8834
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-03-16 16:36:30 +0330

    office expenses list template done

--- core/financial/filters.py
+++ core/financial/filters.py
@@ -1,8 +1,11 @@
 import django_filters
-from django_filters import FilterSet ,   DateFilter
+from django_filters import FilterSet ,DateFilter, ChoiceFilter
 from .models import Financial
 from .models import OfficeExpenses
 from django import forms
+from datetime import datetime, timedelta
+
+
 class FinancialFilter(FilterSet):
 
     class Meta:
@@ -15,9 +18,8 @@ class FinancialFilter(FilterSet):
 
 
 class OfficeExpensesFilter(django_filters.FilterSet):
-    user = django_filters.CharFilter(field_name='user__username', lookup_expr='icontains')
     date = DateFilter(
-        field_name="created_at",
+        field_name="date",
         label="Date (yyyy-mm-dd)",
         method="filter_by_date",
         widget=forms.DateInput(attrs={"type": "date"}),
@@ -25,9 +27,38 @@ class OfficeExpensesFilter(django_filters.FilterSet):
     subject = django_filters.CharFilter(lookup_expr='icontains')
     recipient_name = django_filters.CharFilter(lookup_expr='icontains')
     id = django_filters.NumberFilter(field_name='id')
+
+    date_range = ChoiceFilter(
+            label="Date Range",
+            method="filter_by_date_range",
+            choices=(
+                ("today", "Today"),
+                ("this_week", "This Week"),
+                ("this_month", "This Month"),
+            ),
+        )
     
+
+    def filter_by_date_range(self, queryset, name, value):
+        today = datetime.now().date()
+        if value == "today":
+            return queryset.filter(created_at__date=today)
+        elif value == "this_week":
+            start_of_week = today - timedelta(days=today.weekday())
+            end_of_week = start_of_week + timedelta(days=6)
+            return queryset.filter(created_at__date__range=[start_of_week, end_of_week])
+        elif value == "this_month":
+            start_of_month = today.replace(day=1)
+            end_of_month = start_of_month.replace(
+                month=start_of_month.month + 1, day=1
+            ) - timedelta(days=1)
+            return queryset.filter(
+                created_at__date__range=[start_of_month, end_of_month]
+            )
+
+
     def filter_by_date(self, queryset, name, value):
-        return queryset.filter(created_at__date=value)
+        return queryset.filter(date=value)
     class Meta:
         model = OfficeExpenses
-        fields = ['user', 'date', 'subject', 'recipient_name', 'id']
+        fields = ['user', 'date', 'subject', 'recipient_name', 'id','date_range']
