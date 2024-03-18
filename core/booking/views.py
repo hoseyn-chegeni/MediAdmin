d25e5402599be4a34804cc27ab39e898b6e6fd60
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-03-18 20:28:37 +0330

    package appointment C.U.R.D done

--- core/booking/views.py
+++ core/booking/views.py
@@ -105,7 +105,6 @@ class PackageAppointmentListView(BaseListView):
     context_object_name = "appointments"
     filterset_class = 0
 
-
 class PackageAppointmentCreateView(BaseCreateView):
     model = PackageAppointment
     fields = "__all__"
