2c264e1963e2cc4dc4fd38e80485f60bfbe2944e
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-03-25 10:31:00 +0330

    add clientSMSlog model (#89)

--- core/logs/models.py
+++ core/logs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class ClientSMSLog(models.Model):
+    client = models.ForeignKey('client.Client', on_delete = models.CASCADE)
+    sender_number = models.CharField(max_length=20)
+    receiver_number = models.CharField(max_length=20)
+    subject = models.CharField(max_length = 255)
+    message_body = models.TextField()
+    status = models.CharField(max_length=20)
+    response = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'SMS from {self.sender_number} to {self.client.get_full_name}'
