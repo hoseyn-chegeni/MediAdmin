fa85c6c023c940d1b827676b8f3a97800cfff4c4
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-03-26 18:11:50 +0330

    add C.U.R.D for permission #85

---
+++ core/permissions/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import PermissionsCreateView,PermissionsDeleteView,PermissionsDetailView,PermissionsListView,PermissionsUpdateView,AssignPermissionsView
+
+app_name = 'permissions'
+
+urlpatterns = [
+    path("list/", PermissionsListView.as_view(), name="list"),
+    path("detail/<int:pk>/", PermissionsDetailView.as_view(), name="detail"),
+    path("create/", PermissionsCreateView.as_view(), name="create"),
+    path("update/<int:pk>/", PermissionsUpdateView.as_view(), name="update"),
+    path("delete/<int:pk>/", PermissionsDeleteView.as_view(), name="delete"),
+    path("assign/<int:pk>/", AssignPermissionsView.as_view(), name="assign"),
+]
--- core/permissions/views.py
+++ core/permissions/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from django.contrib.auth.models import Permission
+from base.views import BaseCreateView,BaseDeleteView,BaseDetailView,BaseUpdateView,BaseListView
+from django.views.generic import ListView
+from accounts.models import User
 
 # Create your views here.
+class PermissionsListView(ListView):
+    model = Permission
+    template_name = "permission/list.html"
+    context_object_name = "permission"
+
+
+class PermissionsDetailView(BaseDetailView):
+    model = Permission
+    template_name = "permission/detail.html"
+    permission_required = "permission.view_permission"
+
+
+class PermissionsCreateView(BaseCreateView):
+    model = Permission
+    template_name = "permission/create.html"
+    permission_required = "permission.add_permission"
+    fields = "__all__"
+    app_name = 'permissions'
+    url_name = 'detail'
+    
+    
+class PermissionsUpdateView(BaseUpdateView):
+    model = Permission
+    template_name = "permission/update.html"
+    permission_required = "permission.change_permission"
+    fields = "__all__"
+    app_name = 'permissions'
+    url_name = 'detail'
+
+class PermissionsDeleteView(BaseDeleteView):
+    model = Permission
+    permission_required = "permission.delete_permission"
+    app_name = 'permissions'
+    url_name = 'list'
+
+class AssignPermissionsView(BaseUpdateView):
+    model = User
+    fields = ("user_permissions",)
+    template_name = "permission/assign.html"
+    permission_required = "auth.change_user"
+    app_name = 'accounts'
+    url_name = 'detail'
+
