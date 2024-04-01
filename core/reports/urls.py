0cccffb5d89381d9e08f0eca51ed24b5edccac9e
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-04-01 13:58:40 +0330

    consumable reports download done

--- core/reports/urls.py
+++ core/reports/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import ExportUsersExcelView
+from .views import ExportUsersExcelView, ConsumableReportsView,ExportConsumableExcelView
 
 app_name = 'reports'
 
 urlpatterns = [
         path('export-users-excel/', ExportUsersExcelView.as_view(), name='export_users_excel'),
+
+        path('consumable_reports/', ConsumableReportsView.as_view(), name='consumable_reports'),
+        path('export-consumable-excel/', ExportConsumableExcelView.as_view(), name='export_consumable_excel'),
 ]
--- core/reports/views.py
+++ core/reports/views.py
@@ -5,6 +5,9 @@ from accounts.models import User
 from django.shortcuts import HttpResponse
 import pandas as pd
 
+from asset.filters import ConsumableFilter
+from asset.models import Consumable
+from base.views import BaseListView
 
 # Create your views here.
 class ExportUsersExcelView(View):
@@ -23,6 +26,38 @@ class ExportUsersExcelView(View):
         # Create a response object
         response = HttpResponse(content_type='application/vnd.ms-excel')
         response['Content-Disposition'] = 'attachment; filename="user_report.xlsx"'
+
+        # Write DataFrame to Excel file and return response
+        users_df.to_excel(response, index=False)
+
+        return response
+
+
+
+class ConsumableReportsView(BaseListView):
+    model = Consumable
+    template_name = "reports/consumable_reports.html"
+    context_object_name = "consumable"
+    filterset_class = ConsumableFilter
+    permission_required = "asset.view_consumable"
+    
+
+class ExportConsumableExcelView(View):
+    def get(self, request):
+        # Get filtered users based on request parameters
+        consumable_filter = ConsumableFilter(request.GET, queryset=Consumable.objects.all())
+        filtered_consumable = consumable_filter.qs
+
+        # Convert filtered users queryset to DataFrame
+        users_df = pd.DataFrame(list(filtered_consumable.values()))
+
+        date_columns = users_df.select_dtypes(include=['datetime64[ns, Iran]']).columns
+        for date_column in date_columns:
+            users_df[date_column] = users_df[date_column].dt.date
+
+        # Create a response object
+        response = HttpResponse(content_type='application/vnd.ms-excel')
+        response['Content-Disposition'] = 'attachment; filename="consumable_report.xlsx"'
 
         # Write DataFrame to Excel file and return response
         users_df.to_excel(response, index=False)
