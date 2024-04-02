18cdb3f641bad02326b87f62e52eb258a3440c64
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-04-02 23:30:28 +0330

    rmeove this

--- core/booking/tasks.py
+++ core/booking/tasks.py
@@ -18,6 +18,32 @@ def send_sms_reminders():
     for appointment in appointments_tomorrow:
         try:
             api = KavenegarAPI(
+                getenv('KAVENEGAR_API_KEY')
+            )
+            message = "this is a test from celery "
+            params = {
+                "sender": "2000500666",
+                "receptor": appointment.client.phone_number,
+                "message": message,
+            }
+            response = api.sms_send(params)
+            print("done")
+        except (APIException, HTTPException) as e:
+            print("field")
+
+
+@shared_task
+def send_sms_reminders():
+    # Calculate the date for tomorrow
+    timezone.now()
+
+    # Query appointments scheduled for tomorrow
+    appointments_tomorrow = Appointment.objects.filter(date=timezone.now())
+
+    # Iterate over appointments and send reminders
+    for appointment in appointments_tomorrow:
+        try:
+            api = KavenegarAPI(
                 "344A4E6B3857684B454236343856666C39497A41484D354C584F6C30436C626138506976354B6B4635634D3D"
             )
             message = "this is a test from celery "
