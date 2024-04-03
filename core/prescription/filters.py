b3f7d94801fce13bd171f5f3f008627cbf9107d3
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-04-03 21:27:32 +0330

    prescriptionFilter edit done

--- core/prescription/filters.py
+++ core/prescription/filters.py
@@ -1,11 +1,32 @@
 import django_filters
-from django_filters import FilterSet
+from django_filters import FilterSet,DateFilter
 from .models import Prescription
 from django.db import models
+from base.filters import BaseFilter
+from django import forms
 
 
-class PrescriptionFilter(FilterSet):
+
+
+class PrescriptionFilter(BaseFilter):
+
+
+    date = DateFilter(
+        field_name="date",
+        label="Date (yyyy-mm-dd)",
+        method="filter_by_date_field",
+        widget=forms.DateInput(attrs={"type": "date"}),
+    )
+
+    def filter_by_date_field(self, queryset, name, value):
+        return queryset.filter(date=value)
+
 
     class Meta:
         model = Prescription
-        fields = {"id": ["exact"]}
+        fields = {
+            "id": ["exact"],
+            "reception": ["exact"],
+            "diagnosis": ["exact", 'icontains'],
+            "medication": ["exact", 'icontains']
+        }
