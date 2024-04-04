acbc211ded1f24fb5f78babb96d5af040b049da6
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-04-04 11:45:05 +0330

    add sms filters to template done

--- core/notification/filters.py
+++ core/notification/filters.py
@@ -1,9 +1,21 @@
-from django_filters import FilterSet
+from django_filters import CharFilter
 from logs.models import ClientSMSLog
 from base.filters import BaseFilter
 
 
 class ClientSMSLogFilter(BaseFilter):
+    case_id = CharFilter(
+        field_name="client__case_id", label="Case ID", lookup_expr="exact"
+    )
+
+    # Filter by client national ID
+    national_id = CharFilter(
+        field_name="client__national_id", label="National ID", lookup_expr="exact"
+    )
+
+    # Filter by client name
+    name = CharFilter(label="Client Name", method="filter_by_name")
+
     class Meta:
         model = ClientSMSLog
         fields = {
