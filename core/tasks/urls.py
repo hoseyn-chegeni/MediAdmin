49e974e7fdb456e1c9718ac27c1efd651485d852
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-04-05 15:55:12 +0330

    C.U.R.D for task model

--- core/tasks/urls.py
+++ core/tasks/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
+from .views import TaskCreateView, TaskListView, TaskUpdateView, TaskDeleteView,TaskDetailView
 
 app_name = "tasks"
 
-urlpatterns = []
+urlpatterns = [
+    path("list/", TaskListView.as_view(), name="list"),
+    path("detail/<int:pk>/", TaskDetailView.as_view(), name="detail"),
+    path("create/", TaskCreateView.as_view(), name="create"),
+    path("update/<int:pk>/", TaskUpdateView.as_view(), name="update"),
+    path("delete/<int:pk>/", TaskDeleteView.as_view(), name="delete"),
+]
--- core/tasks/views.py
+++ core/tasks/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
-
+from base.views import BaseListView,BaseCreateView,BaseDeleteView,BaseDetailView,BaseUpdateView
+from .models import Task
 # Create your views here.
+class TaskListView(BaseListView):
+    model = Task
+    template_name = "tasks/list.html"
+    context_object_name = "tasks"
+    filterset_class = 0
+    permission_required = "tasks.view_task"
+
+class TaskDetailView(BaseDetailView):
+    model = Task
+    template_name = "tasks/detail.html"
+    permission_required = "tasks.view_task"
+
+class TaskCreateView(BaseCreateView):
+    model = Task
+    fields = "__all__"
+    template_name = "tasks/create.html"
+    app_name = "tasks"
+    url_name = "detail"
+    permission_required = "tasks.add_task"
+
+class TaskUpdateView(BaseUpdateView):
+    model = Task
+    fields = "__all__"
+    template_name = "tasks/update.html"
+    app_name = "tasks"
+    url_name = "detail"
+    permission_required = "tasks.change_task"
+
+class TaskDeleteView(BaseDeleteView):
+    model = Task
+    app_name = "tasks"
+    url_name = "list"
+    permission_required = "tasks.change_task"
