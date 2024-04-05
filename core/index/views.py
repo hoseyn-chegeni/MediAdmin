10bf08dc830a28d2d53dbac74930126d11810ec7
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-04-05 10:14:16 +0330

    rmeoce index view contxt data beacuse of confilts

--- core/index/views.py
+++ core/index/views.py
@@ -1,22 +1,5 @@
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-from accounts.models import User
-from client.models import Client
-from doctor.models import Doctor
-from services.models import Service, Package
-from booking.models import Appointment, PackageAppointment
-from reception.models import Reception
-from financial.models import Financial, OfficeExpenses
-from logs.models import ClientSMSLog
-from prescription.models import Prescription
-from asset.models import Consumable, Supplier, Equipment
-from insurance.models import Insurance
-from datetime import date
-
-
-
-
-
 
 # Create your views here.
 class IndexView(LoginRequiredMixin, PermissionRequiredMixin, TemplateView):
@@ -24,24 +7,3 @@ class IndexView(LoginRequiredMixin, PermissionRequiredMixin, TemplateView):
     permission_required = "index.view_index"
 
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        today_date = date.today()
-        context["user_count"] = User.objects.count()
-        context["client_count"] = Client.objects.count()
-        context["client_sms_count"] = ClientSMSLog.objects.count()
-        context["appointment_count"] = Appointment.objects.count()
-        context["package_appointment_count"] = PackageAppointment.objects.count()
-        context["todays_appointment_count"] = Appointment.objects.filter(date=today_date).count()
-        context["reception_count"] = Reception.objects.count()
-        context["financial_count"] = Financial.objects.count()
-        context["office_expenses_count"] = OfficeExpenses.objects.count()
-        context["doctor_count"] = Doctor.objects.count()
-        context["service_count"] = Service.objects.count()
-        context["package_count"] = Package.objects.count()
-        context["prescription_count"] = Prescription.objects.count()
-        context["consumable_count"] = Consumable.objects.count()
-        context["supplier_count"] = Supplier.objects.count()
-        context["equipment_count"] = Equipment.objects.count()
-        context["insurance_count"] = Insurance.objects.count()
-        return context
