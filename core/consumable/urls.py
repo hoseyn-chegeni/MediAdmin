50974c5401e229a6935db7acf81a3eb6b15e00c0
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-04-10 14:42:05 +0330

    Consumable C.U.R.D done

--- core/consumable/urls.py
+++ core/consumable/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
+from .views import ConsumableCreateView,ConsumableListView,ConsumableDeleteView,ConsumableDetailView,ConsumableUpdateView
 
 app_name = 'consumable'
 
-urlpatterns =[]
+
+urlpatterns = [
+    path("list/", ConsumableListView.as_view(), name="list"),
+    path("detail/<int:pk>/", ConsumableDetailView.as_view(), name="detail"),
+    path("create/", ConsumableCreateView.as_view(), name="create"),
+    path("update/<int:pk>/", ConsumableUpdateView.as_view(), name="update"),
+    path("delete/<int:pk>/", ConsumableDeleteView.as_view(), name="delete"),
+]
--- core/consumable/views.py
+++ core/consumable/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
-
+from base.views import BaseCreateView,BaseDeleteView,BaseDetailView,BaseUpdateView,BaseListView
+from .models import ConsumableV2
+from django.views.generic import ListView
 # Create your views here.
+#Consumable Views.
+class ConsumableListView(ListView):
+    model = ConsumableV2
+    template_name = "consumable/list.html"
+    context_object_name = "consumable"
+
+
+class ConsumableDetailView(BaseDetailView):
+    model = ConsumableV2
+    template_name = "consumable/detail.html"
+    context_object_name = "consumable"
+    permission_required = "consumable.view_consumablev2"
+
+
+class ConsumableCreateView(BaseCreateView):
+    model = ConsumableV2
+    fields = "__all__"
+    template_name = "consumable/create.html"
+    app_name = "consumable"
+    url_name = "detail"
+    permission_required = "consumable.add_consumablev2"
+
+
+class ConsumableUpdateView(BaseUpdateView):
+    model = ConsumableV2
+    fields = "__all__"
+    template_name = "consumable/update.html"
+    app_name = "consumable"
+    url_name = "detail"
+    permission_required = "consumable.change_consumablev2"
+
+
+class ConsumableDeleteView(BaseDeleteView):
+    model = ConsumableV2
+    app_name = "consumable"
+    url_name = 'list'
+    permission_required = "consumable.delete_consumablev2"
