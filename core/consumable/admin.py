f10dada23c4116142f66b32fe670e8912c520210
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-04-25 10:48:59 +0330

    add supplier link to inventory list in consumable detail page

--- core/consumable/admin.py
+++ core/consumable/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import ConsumableV2, Inventory
+from .models import ConsumableV2, Inventory, Supplier
 
 # Register your models here.
 admin.site.register(ConsumableV2)
 admin.site.register(Inventory)
+admin.site.register(Supplier)
