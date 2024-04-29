3fbc25a10d2422a184ec0d0e313ca30473a28a12
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-04-29 13:54:05 +0330

    add custom calender with tempalte

--- core/planner/urls.py
+++ core/planner/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from .views import CalendarView
 
 app_name = 'planner'
 
 urlpatterns = [
-    
+    path('', CalendarView.as_view(), name='calendar'),
 ]
--- core/planner/views.py
+++ core/planner/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Month
 
 # Create your views here.
+
+class CalendarView(ListView):
+    template_name = 'calendar/calendar.html'
+    context_object_name = 'months'
+
+    def get_queryset(self):
+        return Month.objects.all().prefetch_related('day_set')
