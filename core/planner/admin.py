44d3dbf6bbb5ac5e6092ed0d406db7a946fedf03
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-04-29 13:38:17 +0330

    add week day and month and day model

--- core/planner/admin.py
+++ core/planner/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import WeekDay, Month,Day
 # Register your models here.
+admin.site.register(Day)
+admin.site.register(Month)
+admin.site.register(WeekDay)
--- core/planner/models.py
+++ core/planner/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Month(models.Model):
+    number = models.PositiveIntegerField()
+    slug = models.CharField(max_length = 100)
+     
+    def __str__(self):
+        return self.slug
+    
+
+class Day(models.Model):
+    number = models.PositiveIntegerField()
+    name = models.ForeignKey('WeekDay', on_delete = models.DO_NOTHING)
+    month = models.ForeignKey('Month', on_delete = models.CASCADE)
+    date = models.DateField()
+
+    def __str__(self):
+        return f'{self.month.number} / {self.number}'
+    
+
+
+class WeekDay(models.Model):
+    number = models.PositiveIntegerField()
+    name = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.name
