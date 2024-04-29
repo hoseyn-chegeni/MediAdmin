38f6eb4fa2bc1b2acc6d1cb62ec75c342a36adf2
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-04-29 14:05:07 +0330

    set calendar for all services and show it in service list

--- core/planner/views.py
+++ core/planner/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Month
+from services.models import Service
 
 # Create your views here.
 
 class CalendarView(ListView):
-    template_name = 'calendar/calendar.html'
+    template_name = 'planner/calendar.html'
     context_object_name = 'months'
 
     def get_queryset(self):
         return Month.objects.all().prefetch_related('day_set')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["service"] = Service.objects.get(id=self.kwargs["pk"])
+        return context
