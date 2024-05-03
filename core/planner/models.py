d67acb09335756942a1962d85eefd5f500c2d707
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-05-03 10:29:48 +0330

    add deleted session model

--- core/planner/models.py
+++ core/planner/models.py
@@ -42,3 +42,17 @@ class Session(models.Model):
 
     def get_full_name(self):
         return f"{self.first_name} {self.last_name}"
+
+class DeletedSession(models.Model):
+    day = models.ForeignKey("Day", on_delete=models.CASCADE)
+    service = models.ForeignKey("services.Service", on_delete=models.CASCADE)
+    client = models.ForeignKey(
+        "client.Client", on_delete=models.CASCADE, blank=True, null=True
+    )
+    first_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
+    national_id = models.CharField(max_length=20, blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    reason = models.TextField()
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
