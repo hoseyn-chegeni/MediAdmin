d8bb219ce309b01118f0d96ac6b9c39b6c07fe39
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-05-14 09:48:46 +0330

    add TemporaryPrescription model

--- core/prescription/admin.py
+++ core/prescription/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import PrescriptionHeader, Prescription
+from .models import PrescriptionHeader, Prescription, PrescriptionItem, TemporaryPrescription
 
 # Register your models here.
 admin.site.register(PrescriptionHeader)
 admin.site.register(Prescription)
+admin.site.register(PrescriptionItem)
+admin.site.register(TemporaryPrescription)
+
--- core/prescription/models.py
+++ core/prescription/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 # Create your models here.
+
 class Prescription(models.Model):
     reception = models.ForeignKey("reception.Reception", on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)
@@ -30,7 +31,7 @@ class PrescriptionHeader(models.Model):
     
     
 class PrescriptionItem(models.Model):
-    prescription = models.ForeignKey('Prescription', on_delete = models.CASCADE)
+    prescription = models.ForeignKey('TemporaryPrescription', on_delete = models.CASCADE)
     medicine = models.CharField(max_length = 255) # نام دارو
     quantity = models.PositiveIntegerField() #  تعداد
     consumption_time =  models.CharField(max_length = 255) #  زمان مصرف
@@ -38,3 +39,13 @@ class PrescriptionItem(models.Model):
     how_to_use =  models.CharField(max_length = 255) #طریقه مصرف 
     repeat_interval =  models.CharField(max_length = 255)  #فاصله زمانی تکرار
     repeat_period =  models.PositiveIntegerField() # دوره تکراز
+
+
+class TemporaryPrescription(models.Model):
+    reception = models.ForeignKey("reception.Reception", on_delete=models.CASCADE)
+    notes = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(
+        "accounts.User", on_delete=models.SET_NULL, blank=True, null=True
+    )
