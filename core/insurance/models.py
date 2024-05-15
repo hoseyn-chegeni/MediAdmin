642f48a423bd719871c6a1a7620a85bac6401dab
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-05-15 12:31:29 +0330

    add created_by filed to service insurance model

--- core/insurance/models.py
+++ core/insurance/models.py
@@ -31,6 +31,7 @@ class InsuranceService(models.Model):
     notes = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey('accounts.User', on_delete = models.SET_NULL, blank = True, null = True)
 
     def __str__(self):
         return f"{self.insurance}/{self.service}"
