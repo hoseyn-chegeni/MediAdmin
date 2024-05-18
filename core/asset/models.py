4112318e94df10aafb684a4fa74d4e9372d24a97
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-05-18 12:19:14 +0330

    equipment jalali date done

--- core/asset/models.py
+++ core/asset/models.py
@@ -9,12 +9,12 @@ class Equipment(models.Model):
     manufacturer = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
     serial_number = models.CharField(max_length=50, unique=True)
-    acquisition_date = models.DateField()  # تاریخ خرید
+    acquisition_date = models.CharField(max_length = 255)  # تاریخ خرید
     warranty_expiry_date = models.DateField()
     location = models.CharField(max_length=100)
     is_available = models.BooleanField(default=True)
     description = models.TextField(blank=True, null=True)
-    last_maintenance_date = models.DateField(blank=True, null=True)  # بازدید دوره ای
+    last_maintenance_date = models.CharField(max_length = 255, blank = True, null = True)  # بازدید دوره ای
     created_by = models.ForeignKey(
         "accounts.User", on_delete=models.SET_NULL, blank=True, null=True
     )
