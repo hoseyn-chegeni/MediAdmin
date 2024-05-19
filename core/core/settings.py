d8349a1b76126c09a0b9f7e7d80bb4b35bb57589
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-05-19 15:47:52 +0330

    setup nginx and gunicorn

--- core/core/settings.py
+++ core/core/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1']
 
 
 # Application definition
@@ -150,12 +150,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 if not DEBUG:
-    STATIC_ROOT = "/home/django/www-data/example.com/static/"
+    STATIC_ROOT = "/app/static/"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static/"),
