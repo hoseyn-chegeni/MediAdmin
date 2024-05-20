2ee1fa539d3e1e52d9da8593cfc55cf65b4ba558
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-05-20 16:41:48 +0330

    doctor list dynamic access done

--- core/core/urls.py
+++ core/core/urls.py
@@ -45,4 +45,7 @@ urlpatterns = (
         path("planner/", include("planner.urls")),
         path("import/", include("import.urls")),
     ]
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 )
