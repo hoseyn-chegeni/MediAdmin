cbe0fcf085ea469708cd5700097626b73bd5daa0
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-05-24 08:33:08 +0330

    edit allow host

--- core/core/settings.py
+++ core/core/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1',]
+ALLOWED_HOSTS = ['0.0.0.0','37.152.180.38',]
 
 
 # Application definition
