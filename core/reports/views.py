604e283985980ec915550477f521d58acff98ad9
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-05-26 10:01:24 +0330

    active user done #110

--- core/reports/views.py
+++ core/reports/views.py
@@ -1113,5 +1113,6 @@ class PerformanceManagementReportView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['total_user'] = User.objects.all().count()
+        context['total_users'] = User.objects.all().count()
+        context['active_users'] = User.objects.filter(is_active = True).count()
         return context
