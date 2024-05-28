6b82fa8024d2fcfefdac395a90b2c0b5ba60c0d2
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-05-28 11:03:02 +0330

    (#161)

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -18,9 +18,7 @@ class User(AbstractUser):
         max_length=15, verbose_name="Phone Number", blank=True, null=True
     )
     address = models.TextField(blank=True, null=True)
-    image = models.ImageField(
-        upload_to="images/", default="media/images/default.png", blank=True, null=True
-    )
+    image = models.ImageField(upload_to="images/", blank=True, null=True)
     login_attempts = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- core/core/settings.py
+++ core/core/settings.py
@@ -163,7 +163,6 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = "/app/media/"
-
 MEDIAFILES_DIRS = [
     os.path.join(BASE_DIR, "media/"),
 ]
