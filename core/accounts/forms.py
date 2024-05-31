0e016ead6e5a652707fa4b074c611f7cdb4e1b84
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-05-31 10:52:25 +0330

    user update template done

--- core/accounts/forms.py
+++ core/accounts/forms.py
@@ -40,7 +40,9 @@ class CustomUserCreationForm(UserCreationForm):
 class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = "__all__"
+        fields = [
+            "is_superuser","is_staff","email", "first_name","last_name","date_of_birth","national_id","phone_number","address","image",
+        ]
 
     def clean_first_name(self):
         first_name = self.cleaned_data.get("first_name")
