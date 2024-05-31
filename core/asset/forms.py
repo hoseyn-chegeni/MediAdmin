4eae76d3f3a3ff2da9edf91ae28a0d23350b6f03
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-05-31 14:00:33 +0330

    Test creating equipment with an existing equipment name. #195

--- core/asset/forms.py
+++ core/asset/forms.py
@@ -6,10 +6,14 @@ import re
 class EquipmentForm(forms.ModelForm):
     class Meta:
         model = Equipment
-        exclude = ['serial_number', 'acquisition_date', 'warranty_expiry_date', 'last_maintenance_date']
+        fields ='__all__'
 
     def clean_name(self):
         name = self.cleaned_data.get("name")
+        if Equipment.objects.filter(name=name).exists():
+            raise ValidationError("یک  دستگاه با این نام قبلاً ایجاد شده است.")
+        
         if not re.match(r"^[A-Za-z0-9ا-ی\s]+$", name):
             raise ValidationError("نام باید فقط شامل حروف و اعداد باشد.")
+        
         return name
