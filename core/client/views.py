129a3a36fe803512741906276a3d77f86bedd88a
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-06-01 19:36:49 +0330

    add success message to client bulk delete

--- core/client/views.py
+++ core/client/views.py
@@ -241,7 +241,10 @@ class DeleteSelectedClientView(View):
             client_ids = request.POST.getlist(
                 "client_ids"
             )  # Get the list of selected user IDs from the form
-            Client.objects.filter(id__in=client_ids).delete()  # Delete selected users
+            delete_client_count = Client.objects.filter(id__in=client_ids).delete()  # Delete selected users
+            messages.success(
+                request, f"تعداد {delete_client_count[0]} بیمار با موفقیت حذف شدند."
+            ) 
         return redirect("client:list")
 
 
