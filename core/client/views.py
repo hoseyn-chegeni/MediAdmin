73785068ba5ee3a64534c7553a4f55ce0a2f80d1
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-06-01 14:25:14 +0330

    Test submitting the form with invalid data (e.g., incorrect email format). #210 Test updating client info with special characters in the name. #211

--- core/client/views.py
+++ core/client/views.py
@@ -6,7 +6,7 @@ from base.views import (
     BaseDetailView,
     BaseUpdateView,
 )
-from django.views.generic import ListView, DeleteView
+from django.views.generic import ListView, DeleteView 
 from .models import Client, ClientGallery, ClientAttachment
 from .filters import (
     ClientFilters,
@@ -32,7 +32,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMix
 from django.utils.timezone import now
 from datetime import timedelta
 from django.db.models import Count
-from .forms import ClientCreateForm
+from .forms import ClientCreateForm, ClientUpdateForm
 
 # Create your views here.
 class ClientListView(BaseListView):
@@ -108,21 +108,7 @@ class ClientDeleteView(BaseDeleteView):
 
 class EditPersonalInfoView(BaseUpdateView):
     model = Client
-    fields = [
-        "case_id",
-        "first_name",
-        "last_name",
-        "fathers_name",
-        "national_id",
-        "date_of_birth",
-        "gender",
-        "phone_number",
-        "address",
-        "marital_status",
-        "emergency_contact_name",
-        "emergency_contact_number",
-        "insurance",
-    ]
+    form_class = ClientUpdateForm
     template_name = "client/update.html"
     app_name = "client"
     url_name = "detail"
